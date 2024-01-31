b952b97db3b77c27f5c9998f0ddce7e7942fc71b
Jacobbattsf <126518039+Jacobbattsf@users.noreply.github.com>
2024-01-31 12:45:20 -0600

    Create Main.java
    First Project, helped me gain understanding of variables.

---
+++ JavaBroCodeProjects/Variables/Main.java
@@ -0,0 +1,13 @@
+package Variables;
+
+class Main {
+    public static void main(String[]args){
+    short weight = 165;
+    String sentence = "I found a body on the sea shore";
+    String weightlable="lbs";
+
+    System.out.println(sentence + " and it weighed " + weight + weightlable);
+
+
+    }
+}
