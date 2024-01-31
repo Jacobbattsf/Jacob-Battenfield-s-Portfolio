7793378564a32da8aac716e2825e3f05f55500b0
Jacobbattsf <126518039+Jacobbattsf@users.noreply.github.com>
2024-01-31 14:01:11 -0600

    Create Paths.java
    First time using two files to run a Java Project

---
+++ JavaBroCodeProjects/OptionPain/Paths.java
@@ -0,0 +1,14 @@
+package OptionPain;
+
+public class Paths {
+    public void Path1(){
+        System.out.println("\nYou open the door and find yourself in a blissfull forest with nuggets of gold scattered around your feet.");
+
+    }
+    public void Path2(){
+        System.out.println("\nYou open the door and hellfire surrounds you, you make some new friends but they aren't the best company.");
+    }
+    public void Path3(){
+        System.out.println("\n waiting... waiting waiting... this is boring....");
+    }
+}
