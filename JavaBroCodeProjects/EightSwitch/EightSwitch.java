0bb466ea9058a9f044f8483d8cab8be3e204ded3
Jacobbattsf <jacobbattenfieldsf@gmail.com>
2024-02-01 17:07:52 -0600

    Added EightSwitch
    I added my eightswitch program to my repository. this is my first program committed straight from the github desktop app.

---
+++ JavaBroCodeProjects/EightSwitch/EightSwitch.java
@@ -0,0 +1,38 @@
+//Jacob Battenfield
+//Find me on LinkedIn :)
+//2-1-2024
+//www.linkedin.com/in/jacob-jbatt
+
+package EightSwitch;
+import java.util.*;
+public class EightSwitch {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+    System.out.println("what is your greatest wish");
+
+    String wish = scanner.nextLine();
+    int randomInt = random.nextInt(8)+1;
+    scanner.close();
+    System.out.println("So your wish is "+wish);
+
+    switch(randomInt){
+        case 1:System.out.println("It will never happen");      
+        break;
+        case 2:System.out.println("Not very likely to happen");      
+        break;
+        case 3:System.out.println("No chance");     
+        break;
+        case 4:System.out.println("It Could happen");      
+        break;
+        case 5:System.out.println("It is probable");       
+        break;
+        case 6:System.out.println("It is certain");      
+        break;
+        case 7:System.out.println("It is written in the stars");
+        break;
+        case 8:System.out.println("It will 1000% work, trust me bro");       
+        break;
+    }
+    }
+}
