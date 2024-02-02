fb2ec0a3ad03c462772583a7154227de4b4bf399
Jacobbattsf <jacobbattenfieldsf@gmail.com>
2024-02-01 21:52:36 -0600

    Commit to bigjav

---
+++ JavaBigCode/Jgame.java
@@ -0,0 +1,10 @@
+//Begining of my game, this should be good.
+
+import javax.swing.JFrame;
+
+public class Jgame{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+    }
+}
